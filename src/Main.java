a408387eb1cb04a6dda08f4252707a5a7fd31c9f
Влад Тычинский <99480327+vladclown@users.noreply.github.com>
2023-03-06 16:10:44 +0500

    second line added

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        
+        
 
             System.out.println("Введите своё число");
+        
+            System.out.println("Введите своё число");
+        
             int value = new Scanner(System.in).nextInt();
             for (int i = 1; i <= value; i = i + 1) {
                 int solution = value * i;
